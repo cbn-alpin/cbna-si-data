5d60cf245c35d840965f609136cfbebf77d860bf
Christine BIONDA <c.bionda@cbn-alpin.fr>
2023-08-03 14:26:07 +0200

    fix simethis synthese and add function generate_observers to simethis synthese

--- simethis/data/sql/synthese.sql
+++ simethis/data/sql/synthese.sql
@@ -1,3 +1,135 @@
+CREATE OR REPLACE FUNCTION flore.generate_observers(id_releve integer, sep character varying DEFAULT ' '::character varying)
+ RETURNS text
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+DECLARE
+generate_observers text;
+begin
+generate_observers :=
+
+concat_ws($2,
+	  	CASE 
+			WHEN rel.id_obs1 IS NOT NULL --obs1
+				THEN concat(obs1.nom,' ', obs1.prenom, 
+					CASE
+						WHEN obs1.email IS NOT NULL AND obs1.email != '' --si email obs1
+							THEN concat(' <', obs1.email,'>') --email obs1
+						ELSE ''
+					END,
+					' (',
+			  		CASE
+						WHEN id_org_obs1 IS NOT NULL --si org1
+							THEN (SELECT coalesce( NULLIF(org1.abb,'') ,org1.nom) --org_obs1
+								FROM referentiels.organisme org1
+								WHERE rel.id_org_obs1 = org1.id_org )
+						ELSE coalesce(NULLIF(orgf.abb,''),orgf.nom)
+					END,')',
+					concat(' [',uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', rel.id_obs1)),']') -- uuid obs1
+						   )
+			ELSE NULL
+		END,		
+        CASE 
+			WHEN rel.id_obs2 IS NOT NULL --obs2
+				THEN concat(obs2.nom,' ', obs2.prenom, 
+					CASE
+						WHEN obs2.email IS NOT NULL AND obs2.email != '' --si email obs2
+							THEN concat(' <', obs2.email,'>') --email obs2
+						ELSE ''
+					END,
+					' (',
+			  		CASE
+						WHEN id_org_obs2 IS NOT NULL --si org2
+							THEN (SELECT coalesce( NULLIF(org2.abb,'') ,org2.nom) --org_obs2
+								FROM referentiels.organisme org2
+								WHERE rel.id_org_obs2 = org2.id_org )
+						ELSE coalesce(NULLIF(orgf.abb,''),orgf.nom)
+					END,')',
+					concat(' [',uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', rel.id_obs2)),']') -- uuid obs2
+						   )
+			ELSE NULL
+		END,
+		CASE 
+			WHEN rel.id_obs3 IS NOT NULL --obs3
+				THEN concat(obs3.nom,' ', obs3.prenom, 
+					CASE
+						WHEN obs3.email IS NOT NULL AND obs3.email != '' --si email obs3
+							THEN concat(' <', obs3.email,'>') --email obs3
+						ELSE ''
+					END,
+					' (',
+			  		CASE
+						WHEN id_org_obs3 IS NOT NULL --si org3
+							THEN (SELECT coalesce( NULLIF(org3.abb,'') ,org3.nom) --org_obs3
+								FROM referentiels.organisme org3
+								WHERE rel.id_org_obs3 = org3.id_org )
+						ELSE coalesce(NULLIF(orgf.abb,''),orgf.nom)
+					END,')',
+					concat(' [',uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', rel.id_obs3)),']') -- uuid obs3
+						   )
+			ELSE NULL
+		END,
+		CASE 
+			WHEN rel.id_obs4 IS NOT NULL --obs4
+				THEN concat(obs4.nom,' ', obs4.prenom, 
+					CASE
+						WHEN obs4.email IS NOT NULL AND obs4.email != '' --si email obs4
+							THEN concat(' <', obs4.email,'>') --email obs4
+						ELSE ''
+					END,
+					' (',
+			  		CASE
+						WHEN id_org_obs4 IS NOT NULL --si org4
+							THEN (SELECT coalesce( NULLIF(org4.abb,'') ,org4.nom) --org_obs4
+								FROM referentiels.organisme org4
+								WHERE rel.id_org_obs4 = org4.id_org )
+						ELSE coalesce(NULLIF(orgf.abb,''),orgf.nom)
+					END,')',
+					concat(' [',uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', rel.id_obs4)),']') -- uuid obs4
+						   )
+			ELSE NULL
+		END,
+		CASE 
+			WHEN rel.id_obs5 IS NOT NULL --obs5
+				THEN concat(obs5.nom,' ', obs5.prenom, 
+					CASE
+						WHEN obs5.email IS NOT NULL AND obs5.email != '' --si email obs5
+							THEN concat(' <', obs5.email,'>') --email obs5
+						ELSE ''
+					END,
+					' (',
+			  		CASE
+						WHEN id_org_obs5 IS NOT NULL --si org5
+							THEN (SELECT coalesce( NULLIF(org5.abb,'') ,org5.nom) --org_obs5
+								FROM referentiels.organisme org5
+								WHERE rel.id_org_obs5 = org5.id_org )
+						ELSE coalesce(NULLIF(orgf.abb,''),orgf.nom)
+					END,')',
+					concat(' [',uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', rel.id_obs5)),']') -- uuid obs5
+						   )
+			ELSE NULL
+		END
+		)
+
+
+	FROM flore.releve rel
+		LEFT JOIN referentiels.observateur obs1 on rel.id_obs1 = obs1.id_obs
+    	LEFT JOIN referentiels.observateur obs2 on rel.id_obs2 = obs2.id_obs
+		LEFT JOIN referentiels.observateur obs3 on rel.id_obs3 = obs3.id_obs
+		LEFT JOIN referentiels.observateur obs4 on rel.id_obs4 = obs4.id_obs
+		LEFT JOIN referentiels.observateur obs5 on rel.id_obs5 = obs5.id_obs
+		LEFT JOIN referentiels.organisme orgf on rel.id_org_f = orgf.id_org
+	WHERE rel.id_releve = $1;
+	
+return generate_observers;
+
+END;
+$function$
+;
+
+
+
+-- Requête export_synthese_initialisation
 --CREATE TEMP TABLE temp_det_org
 --AS
 --SELECT 
@@ -157,7 +289,7 @@ r.date_releve_fin AS date_max,
     NULL::text AS validation_comment,
     COALESCE(o.meta_date_valid, o.meta_date_maj) AS validation_date,
 --flore.obs_plus_orga(r.id_releve, ', '::character varying) AS observers,
---flore.obs_org_email_uuid(r.id_releve, ', '::character varying) AS observers,  
+flore.generate_observers(r.id_releve, ', '::character varying) AS observers,  
 concat(
 	CASE 
 		WHEN o.id_det IS NOT NULL 
@@ -307,6 +439,7 @@ WHERE
 		OR  (r.meta_id_groupe <> 1 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74')))
 --DROP TABLE temp_det_org;
 --limit 100
+;
 
 
 
