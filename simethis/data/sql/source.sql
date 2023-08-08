56efbfe45473cb1cb535660b1896cde0625990f1
Christine BIONDA <c.bionda@cbn-alpin.fr>
2023-08-08 16:52:45 +0200

    fix simethis source : name, desc, meta_create_date, meta_update_date, entity_source_pk_field

--- simethis/data/sql/source.sql
+++ simethis/data/sql/source.sql
@@ -1,43 +1,59 @@
---WITH
-	
+WITH
+	others_cbn AS ( 
+	SELECT DISTINCT ON (o.nom)
+	o.meta_date_saisie, 
+	o.nom,
+	o.meta_date_maj 
+	FROM flore.releve r 
+	JOIN referentiels.organisme o ON o.id_org = r.id_org_f 
+	WHERE r.id_org_f IS NOT NULL AND r.meta_id_groupe <> 1 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74')
+	),
+	partners_cbna AS ( 
+	SELECT DISTINCT ON (o.nom)
+	o.nom,
+	o.meta_date_saisie,
+	o.meta_date_maj
+	FROM flore.releve r 
+	JOIN referentiels.organisme o ON o.id_org = r.id_org_f 
+	WHERE r.id_org_f IS NOT NULL AND r.meta_id_groupe = 1 AND r.id_org_f <> 2785 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74')
+	)
+
 SELECT
- 	'simethis'::TEXT AS name,
-    'Observations issues de Simethis, la base de données mutualisée des CBN Alpin, Corse et Méditerranéen correspondant à leur observations propres et à celles de leurs partenaires (Parcs nationaux, parcs naturels régionaux, CEN, bureaux d''études, …)'::text AS "desc",
-    'id_observation'::text AS entity_source_pk_field,
+ 	oc.nom::TEXT AS name,
+    'Observations issues de Simethis et produites par les CBN Corse et Méditerranéen et leurs partenaires (Parcs nationaux, parcs naturels régionaux, CEN, bureaux d''études, …)'::text AS "desc",
+    NULL::text AS entity_source_pk_field,
     NULL::character varying AS url,
     NULL::jsonb AS additional_data,
-    '2021-02-24'::timestamp AS meta_create_date,
-    now()::timestamp AS meta_update_date,
+    oc.meta_date_saisie::timestamp AS meta_create_date,
+    oc.meta_date_maj::timestamp AS meta_update_date,
     'I'::character(1) AS meta_last_action
+FROM others_cbn oc
     
 UNION
-    
-SELECT
-	'fournisseurs'::TEXT AS name,
-	'Observations issus de bases autre que Simethis à l''origine' AS "desc",
-	'id_observation'::text AS entity_source_pk_field,
+
+SELECT   
+	pc.nom::TEXT AS name,
+	'Observations produites par les partenaires du CBNA' AS "desc",
+	NULL::text AS entity_source_pk_field,
     NULL::character varying AS url,
     NULL::jsonb AS additional_data,
-    '2021-02-24'::timestamp AS meta_create_date,
-    now()::timestamp AS meta_update_date,
+    pc.meta_date_saisie::timestamp AS meta_create_date,
+    pc.meta_date_maj::timestamp AS meta_update_date,
     'I'::character(1) AS meta_last_action
-    
-    
+FROM partners_cbna pc
+
 UNION 
 
 SELECT 
 	'flora_cbna'::TEXT AS name,
-	'Observations issus du CBNA' AS "desc",
-	'id_observation'::text AS entity_source_pk_field,
+	'Observations produites par le CBNA' AS "desc",
+	NULL::text AS entity_source_pk_field,
     NULL::character varying AS url,
     NULL::jsonb AS additional_data,
-    '2021-02-24'::timestamp AS meta_create_date,
-    now()::timestamp AS meta_update_date,
+    '2021-01-28'::timestamp AS meta_create_date,
+    '2023-04-21'::timestamp AS meta_update_date,
     'I'::character(1) AS meta_last_action
     
- 
-  
     
     
-    
- 
+   
