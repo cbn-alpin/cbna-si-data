d96e9b4e07e4797989c582b9ce7288370e7d4765
Christine BIONDA <c.bionda@cbn-alpin.fr>
2023-08-18 18:39:01 +0200

    simethis user : add create and drop table cbna_agent, add functions delate_space and check_cbna_agent, fix CTE

--- simethis/data/sql/user.sql
+++ simethis/data/sql/user.sql
@@ -1,10 +1,43 @@
 --COPY ( 
+
+-- Création d'une table, qui sera supprimée en fin de script, des agents du CBNA des services Conservation et Connaissance
+CREATE TABLE flore.cbna_agent(
+	gid serial PRIMARY KEY,
+	uuid uuid,
+	last_name varchar(100),
+	first_name varchar(100),
+	entry_date date,
+	release_date date
+);
+
+-- Insertion des données du fichier CSV dans la table
+COPY flore.cbna_agent(last_name, first_name, entry_date, release_date)
+	FROM '/home/cbionda/workspace/geonature/migration_data_simethis/cbna-si-data/geonature/data/raw/cbna_agent.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Récupération des uuid des agents
+UPDATE flore.cbna_agent AS ca
+SET uuid = u.permid
+FROM applications.utilisateur AS u
+WHERE u.id_groupe = 1 AND lower(unaccent(u.nom)) = lower(unaccent(ca.last_name)) AND lower(unaccent(u.prenom)) = lower(unaccent(ca.first_name));
+
 (
 WITH
--- CTE Relevés d'observations produites sur le territoire du CBNA
-	releves_siAlp AS( 
-		SELECT rel.id_releve 
+-- CTE Relevés d'observations produites sur le territoire du CBNA, observateurs du territoire du CBNA
+	releves_sialp AS( 
+		SELECT 
+			rel.id_releve,
+			rel.date_releve_deb,
+			rel.date_releve_fin,
+			rel.id_obs1, 
+			rel.id_obs2,
+			rel.id_obs3,
+			rel.id_obs4,
+			rel.id_obs5, 
+			COALESCE(org.uuid_national, org.permid::varchar) AS code_organism
 		FROM flore.releve rel
+			LEFT JOIN referentiels.organisme org ON org.id_org = rel.id_org_f
 		WHERE
 		(rel.meta_id_groupe = 1
 			OR  (rel.meta_id_groupe <> 1 AND rel.insee_dept IN ('04', '05', '01', '26', '38', '73', '74')))
@@ -14,62 +47,35 @@ SELECT DISTINCT ON (observers.unique_id) *
 FROM (  	
 
 	SELECT
-	 	concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs1)))::uuid AS unique_id, -- obs1
+--	 	concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs1)))::uuid AS unique_id, -- obs1
+		flore.check_cbna_agent(r.id_obs1, r.date_releve_deb) AS unique_id,
 	 	NULL AS identifier,
-	    CASE 
-	    	WHEN o.prenom IS NOT NULL AND o.prenom !~* '^\s*$'
-	    		THEN o.prenom
-	    	ELSE NULL
-	    END AS firstname,
+	    public.delete_space(o.prenom) AS firstname,
 	    o.nom AS "name",
-	    CASE 
-	    	WHEN o.email IS NOT NULL AND o.email !~* '^\s*$'
-	    		THEN o.email
-	    	ELSE NULL
-	    END AS email,
-	    COALESCE(org.uuid_national, org.permid::varchar) AS code_organism,
-	    CASE 
-	    	WHEN o.comm IS NOT NULL AND o.comm !~* '^\s*$'
-	    		THEN o.comm
-	    	ELSE NULL
-	    END AS "comment",
+	    public.delete_space(o.email) AS email,
+	    r.code_organism,
+	    public.delete_space(o.comm) AS "comment",
 	    NULL::boolean AS "enable",
 	    jsonb_build_object(
 	    	'code', o.code, 'idGroupe', o.meta_id_groupe, 'anneeNai', o.annee_nai , 'anneeDec', o.annee_dec
 	    )::jsonb AS additional_data,
 	    o.meta_date_saisie::timestamp AS meta_create_date,
 	    o.meta_date_maj::timestamp AS meta_update_date,
-	    'I'::char(1) AS meta_last_action
-	FROM flore.releve r
-		JOIN referentiels.observateur o ON o.id_obs = r.id_obs1
-		LEFT JOIN referentiels.organisme org ON org.id_org = r.id_org_f
---	WHERE
---		(r.meta_id_groupe = 1
---			OR  (r.meta_id_groupe <> 1 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74'))) AND r.id_obs1 IS NOT NULL
-		JOIN releves_siAlp relsi ON relsi.id_releve = r.id_releve 
+	    'I'::char(1) AS meta_last_action   	
+	FROM releves_sialp AS r
+		JOIN referentiels.observateur o ON o.id_obs = r.id_obs1 
 		
 	UNION
 	
 	SELECT
-	    concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs2)))::uuid AS unique_id, --obs2
+--	    concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs2)))::uuid AS unique_id, --obs2
+		flore.check_cbna_agent(r.id_obs2, r.date_releve_deb ) AS unique_id,
 	 	NULL AS identifier,
-	    CASE 
-	    	WHEN o.prenom IS NOT NULL AND o.prenom !~* '^\s*$'
-	    		THEN o.prenom
-	    	ELSE NULL
-	    END AS firstname,
+	    public.delete_space(o.prenom) AS firstname,
 	    o.nom AS "name",
-	    CASE 
-	    	WHEN o.email IS NOT NULL AND o.email !~* '^\s*$'
-	    		THEN o.email
-	    	ELSE NULL
-	    END AS email,
-	    COALESCE(org.uuid_national, org.permid::varchar) AS code_organism,
-	    CASE 
-	    	WHEN o.comm IS NOT NULL AND o.comm !~* '^\s*$'
-	    		THEN o.comm
-	    	ELSE NULL
-	    END AS "comment",
+	    public.delete_space(o.email) AS email,
+	    r.code_organism,
+	    public.delete_space(o.comm) AS "comment",
 	    NULL::boolean AS "enable",
 	    jsonb_build_object(
 	    	'code', o.code, 'idGroupe', o.meta_id_groupe, 'anneeNai', o.annee_nai , 'anneeDec', o.annee_dec
@@ -77,36 +83,20 @@ FROM (
 	    o.meta_date_saisie::timestamp AS meta_create_date,
 	    o.meta_date_maj::timestamp AS meta_update_date,
 	    'I'::char(1) AS meta_last_action
-	FROM flore.releve r
+	FROM releves_sialp AS r
 		JOIN referentiels.observateur o ON o.id_obs = r.id_obs2
-		LEFT JOIN referentiels.organisme org ON org.id_org = r.id_org_f
---	WHERE
---		(r.meta_id_groupe = 1
---			OR  (r.meta_id_groupe <> 1 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74'))) AND r.id_obs2 IS NOT NULL
-		JOIN releves_siAlp relsi ON relsi.id_releve = r.id_releve 
 	
 	UNION
 	
 	SELECT
-	 	concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs3)))::uuid AS unique_id, --obs3
+--	 	concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs3)))::uuid AS unique_id, --obs3
+		flore.check_cbna_agent(r.id_obs3, r.date_releve_deb) AS unique_id,
 	 	NULL AS identifier,
-	    CASE 
-	    	WHEN o.prenom IS NOT NULL AND o.prenom !~* '^\s*$'
-	    		THEN o.prenom
-	    	ELSE NULL
-	    END AS firstname,
+	    public.delete_space(o.prenom) AS firstname,
 	    o.nom AS "name",
-	    CASE 
-	    	WHEN o.email IS NOT NULL AND o.email !~* '^\s*$'
-	    		THEN o.email
-	    	ELSE NULL
-	    END AS email,
-	    COALESCE(org.uuid_national, org.permid::varchar) AS code_organism,
-	    CASE 
-	    	WHEN o.comm IS NOT NULL AND o.comm !~* '^\s*$'
-	    		THEN o.comm
-	    	ELSE NULL
-	    END AS "comment",
+	    public.delete_space(o.email) AS email,
+	    r.code_organism,
+	    public.delete_space(o.comm) AS "comment",
 	    NULL::boolean AS "enable",
 	    jsonb_build_object(
 	    	'code', o.code, 'idGroupe', o.meta_id_groupe, 'anneeNai', o.annee_nai , 'anneeDec', o.annee_dec
@@ -114,36 +104,20 @@ FROM (
 	    o.meta_date_saisie::timestamp AS meta_create_date,
 	    o.meta_date_maj::timestamp AS meta_update_date,
 	    'I'::char(1) AS meta_last_action
-	FROM flore.releve r
-		JOIN referentiels.observateur o ON o.id_obs = r.id_obs3
-		LEFT JOIN referentiels.organisme org ON org.id_org = r.id_org_f
---	WHERE
---		(r.meta_id_groupe = 1
---			OR  (r.meta_id_groupe <> 1 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74'))) AND r.id_obs3 IS NOT NULL
-		JOIN releves_siAlp relsi ON relsi.id_releve = r.id_releve 
+	FROM releves_sialp AS r
+		JOIN referentiels.observateur o ON o.id_obs = r.id_obs3 
 	
 	UNION
 	
 	SELECT
-	 	concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs4)))::uuid AS unique_id, --obs4
+--	 	concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs4)))::uuid AS unique_id, --obs4
+		flore.check_cbna_agent(r.id_obs4, r.date_releve_deb) AS unique_id,
 	 	NULL AS identifier,
-	    CASE 
-	    	WHEN o.prenom IS NOT NULL AND o.prenom !~* '^\s*$'
-	    		THEN o.prenom
-	    	ELSE NULL
-        END AS firstname,
+        public.delete_space(o.prenom) AS firstname,
 	    o.nom AS "name",
-	    CASE 
-	    	WHEN o.email IS NOT NULL AND o.email !~* '^\s*$'
-	    		THEN o.email
-	    	ELSE NULL
-	    END AS email,
-	    COALESCE(org.uuid_national, org.permid::varchar) AS code_organism,
-	    CASE 
-	    	WHEN o.comm IS NOT NULL AND o.comm !~* '^\s*$'
-	    		THEN o.comm
-	    	ELSE NULL
-	    END AS "comment",
+	    public.delete_space(o.email) AS email,
+	    r.code_organism,
+	    public.delete_space(o.comm) AS "comment",
 	    NULL::boolean AS "enable",
 	    jsonb_build_object(
 	    	'code', o.code, 'idGroupe', o.meta_id_groupe, 'anneeNai', o.annee_nai , 'anneeDec', o.annee_dec
@@ -151,36 +125,20 @@ FROM (
 	    o.meta_date_saisie::timestamp AS meta_create_date,
 	    o.meta_date_maj::timestamp AS meta_update_date,
 	    'I'::char(1) AS meta_last_action
-	FROM flore.releve r
-		JOIN referentiels.observateur o ON o.id_obs = r.id_obs4
-		LEFT JOIN referentiels.organisme org ON org.id_org = r.id_org_f
---	WHERE
---		(r.meta_id_groupe = 1
---			OR  (r.meta_id_groupe <> 1 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74'))) AND r.id_obs4 IS NOT NULL
-		JOIN releves_siAlp relsi ON relsi.id_releve = r.id_releve 
+	FROM releves_sialp AS r
+		JOIN referentiels.observateur o ON o.id_obs = r.id_obs4 
 		
 	UNION
 	
 	SELECT
-	 	concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs5)))::uuid AS unique_id, --obs5
+--	 	concat(uuid_generate_v5(uuid_ns_url(), concat('https://simethis.eu/referentiels/observateur/', r.id_obs5)))::uuid AS unique_id, --obs5
+		flore.check_cbna_agent(r.id_obs5, r.date_releve_deb) AS unique_id,
 	 	NULL AS identifier,
-	    CASE 
-	    	WHEN o.prenom IS NOT NULL AND o.prenom !~* '^\s*$'
-	    		THEN o.prenom
-	    	ELSE NULL
-	    END AS firstname,
+	    public.delete_space(o.prenom) AS firstname,
 	    o.nom AS "name",
-	    CASE 
-	    	WHEN o.email IS NOT NULL AND o.email !~* '^\s*$'
-	    		THEN o.email
-	    	ELSE NULL
-	    END AS email,
-	    COALESCE(org.uuid_national, org.permid::varchar) AS code_organism,
-	    CASE 
-	    	WHEN o.comm IS NOT NULL AND o.comm !~* '^\s*$'
-	    		THEN o.comm
-	    	ELSE NULL
-	    END AS "comment",
+	    public.delete_space(o.email) AS email,
+	    r.code_organism,
+	    public.delete_space(o.comm) AS "comment",
 	    NULL::boolean AS "enable",
 	    jsonb_build_object(
 	    	'code', o.code, 'idGroupe', o.meta_id_groupe, 'anneeNai', o.annee_nai , 'anneeDec', o.annee_dec
@@ -188,13 +146,9 @@ FROM (
 	    o.meta_date_saisie::timestamp AS meta_create_date,
 	    o.meta_date_maj::timestamp AS meta_update_date,
 	    'I'::char(1) AS meta_last_action
-	FROM flore.releve r
+	FROM releves_sialp AS r
 		JOIN referentiels.observateur o ON o.id_obs = r.id_obs5
-		LEFT JOIN referentiels.organisme org ON org.id_org = r.id_org_f
---	WHERE
---		(r.meta_id_groupe = 1
---			OR  (r.meta_id_groupe <> 1 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74'))) AND r.id_obs5 IS NOT NULL
-		JOIN releves_siAlp relsi ON relsi.id_releve = r.id_releve 
+		
 	) AS observers
 
 UNION
@@ -204,23 +158,11 @@ UNION
 SELECT DISTINCT ON (u.permid)
  	u.permid AS unique_id,
  	u.login AS identifier,
-    CASE 
-    	WHEN u.prenom IS NOT NULL AND u.prenom !~* '^\s*$'
-    		THEN u.prenom
-    	ELSE NULL
-    END AS firstname,
+    public.delete_space(u.prenom) AS firstname,
     u.nom AS "name",
-    CASE 
-    	WHEN u.email IS NOT NULL AND u.email !~* '^\s*$'
-    		THEN u.email
-    	ELSE NULL
-    END AS email,
+    public.delete_space(u.email) AS email,
     COALESCE(o.uuid_national, o.permid::varchar) AS code_organism, 
-    CASE 
-    	WHEN u.comm IS NOT NULL AND u.comm !~* '^\s*$'
-    		THEN u.comm
-    	ELSE NULL
-    END AS "comment",
+    public.delete_space(u.comm) AS "comment",
     NULL::boolean AS "enable",
     jsonb_build_object(
     	'code', u.code, 'idGroupe', u.id_groupe, 'password', u.pass, 'lastLogin', u.last_login, 'key', u."key"
@@ -240,23 +182,11 @@ UNION
 SELECT DISTINCT ON (u.permid) 
  	u.permid AS unique_id,
  	u.login AS identifier,
-    CASE 
-    	WHEN u.prenom IS NOT NULL AND u.prenom !~* '^\s*$'
-    		THEN u.prenom
-    	ELSE NULL
-    END AS firstname,
+    public.delete_space(u.prenom) AS firstname,
     u.nom AS "name",
-    CASE 
-    	WHEN u.email IS NOT NULL AND u.email !~* '^\s*$'
-    		THEN u.email
-    	ELSE NULL
-    END AS email,
+    public.delete_space(u.email) AS email,
     COALESCE(o.uuid_national, o.permid::varchar) AS code_organism, 
-    CASE 
-    	WHEN u.comm IS NOT NULL AND u.comm !~* '^\s*$'
-    		THEN u.comm
-    	ELSE NULL
-    END AS "comment",
+    public.delete_space(u.comm) AS "comment",
     NULL::boolean AS "enable",
     jsonb_build_object(
     	'code', u.code, 'idGroupe', u.id_groupe, 'password', u.pass, 'lastLogin', u.last_login, 'key', u."key"
@@ -274,3 +204,5 @@ WHERE
 --) TO stdout
 --WITH (format csv, header, delimiter E'\t')			
 ;
+
+DROP TABLE IF EXISTS flore.cbna_agent;
