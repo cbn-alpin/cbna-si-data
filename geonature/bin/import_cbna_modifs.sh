54ab8c7c824e9d4e7f3cc82807158432af9820da
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2023-08-23 17:17:34 +0200

    feat geonature bin: add script to import cbna taxref modifs

---
+++ geonature/bin/import_cbna_modifs.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# Encoding : UTF-8
+# Import some updated, deleted or new data in GeoNature Database the CBNA Flora Data.
+
+#+----------------------------------------------------------------------------------------------------------+
+# Configure script execute options
+set -euo pipefail
+
+# DESC: Usage help
+# ARGS: None
+# OUTS: None
+function printScriptUsage() {
+    cat << EOF
+Usage: ./$(basename $BASH_SOURCE)[options]
+     -h | --help: display this help
+     -v | --verbose: display more infos
+     -x | --debug: display debug script infos
+     -c | --config: path to config file to use (default : config/settings.ini)
+EOF
+    exit 0
+}
+
+# DESC: Parameter parser
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: Variables indicating command-line parameters and options
+function parseScriptOptions() {
+    # Transform long options to short ones
+    for arg in "${@}"; do
+        shift
+        case "${arg}" in
+            "--help") set -- "${@}" "-h" ;;
+            "--verbose") set -- "${@}" "-v" ;;
+            "--debug") set -- "${@}" "-x" ;;
+            "--config") set -- "${@}" "-c" ;;
+            "--"*) exitScript "ERROR : parameter '${arg}' invalid ! Use -h option to know more." 1 ;;
+            *) set -- "${@}" "${arg}"
+        esac
+    done
+
+    while getopts "hvxc:" option; do
+        case "${option}" in
+            "h") printScriptUsage ;;
+            "v") readonly verbose=true ;;
+            "x") readonly debug=true; set -x ;;
+            "c") setting_file_path="${OPTARG}" ;;
+            *) exitScript "ERROR : parameter invalid ! Use -h option to know more." 1 ;;
+        esac
+    done
+}
+
+# DESC: Main control flow
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: None
+function main() {
+        #+----------------------------------------------------------------------------------------------------------+
+    # Load utils
+    source "$(dirname "${BASH_SOURCE[0]}")/../../shared/lib/utils.bash"
+
+    #+----------------------------------------------------------------------------------------------------------+
+    # Init script
+    initScript "${@}"
+    parseScriptOptions "${@}"
+    loadScriptConfig "${setting_file_path-}"
+    redirectOutput "${cbna_log_imports}"
+
+    #+----------------------------------------------------------------------------------------------------------+
+    # Start script
+    printInfo "${app_name} script started at: ${fmt_time_start}"
+
+    buildTablePrefix
+
+    executeCopy "taxref rangs"
+    executeCopy "taxref modifs"
+
+    read -r -n 1 key # Move to a new line
+
+    #+----------------------------------------------------------------------------------------------------------+
+    # Display script execution infos
+    displayTimeElapsed
+}
+
+function buildTablePrefix() {
+    table_prefix="${app_code}_${sialp_import_date//-/}"
+}
+
+function executeCopy() {
+    if [[ $# -lt 1 ]]; then
+        exitScript "Missing required argument to ${FUNCNAME[0]}()!" 2
+    fi
+
+    local type="${1,,}"
+
+    local data_type="${type// /_}"
+    local data_type_abbr=$(echo "${type}" | sed 's/\(.\)[^ ]* */\1/g')
+    if [[ "${#data_type_abbr}" = "1" ]]; then
+        data_type_abbr="${data_type}"
+    fi
+
+    # Exit if CSV file not found
+    if ! [[ -f "${raw_dir}/${sialp_import_date}_${data_type}.csv" ]]; then
+        return 0
+    fi
+
+    local table="${table_prefix}_${data_type}"
+    local sql_file="${sql_dir}/${data_type}/copy.sql"
+    local psql_var="${data_type_abbr}ImportTable"
+
+    printMsg "Copy ${type^^} in GeoNature database..."
+    sudo -n -u "${pg_admin_name}" -s \
+        psql -d "${db_name}" \
+            -v "${psql_var}=${table}" \
+            -v gnDbOwner="${db_user}" \
+            -v csvFilePath="${raw_dir}/${sialp_import_date}_${data_type}.csv" \
+            -f "${sql_file}"
+}
+
+
+main "${@}"
