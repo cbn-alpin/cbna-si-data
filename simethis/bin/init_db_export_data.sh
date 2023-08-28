153a37ea55f5986ac66918cec5f66426d9b9ddd4
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2023-08-28 17:08:05 +0200

    feat(simethis bin): create new script bash with functions

---
+++ simethis/bin/init_db_export_data.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Encoding : UTF-8
+# Import some updated, deleted or new data in GeoNature Database the CBNA Flora Data.
+
+#+----------------------------------------------------------------------------------------------------------+
+# Configure script execute options
+set -euo pipefail
+
+# DESC: Usage help
+# ARGS: None
+# OUTS: None
+function printScriptUsage() {
+    cat << EOF
+Usage: ./$(basename $BASH_SOURCE)[options]
+     -h | --help: display this help
+     -v | --verbose: display more infos
+     -x | --debug: display debug script infos
+     -c | --config: path to config file to use (default : config/settings.ini)
+EOF
+    exit 0
+}
+
+# DESC: Parameter parser
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: Variables indicating command-line parameters and options
+function parseScriptOptions() {
+    # Transform long options to short ones
+    for arg in "${@}"; do
+        shift
+        case "${arg}" in
+            "--help") set -- "${@}" "-h" ;;
+            "--verbose") set -- "${@}" "-v" ;;
+            "--debug") set -- "${@}" "-x" ;;
+            "--config") set -- "${@}" "-c" ;;
+            "--"*) exitScript "ERROR : parameter '${arg}' invalid ! Use -h option to know more." 1 ;;
+            *) set -- "${@}" "${arg}"
+        esac
+    done
+
+    while getopts "hvxc:" option; do
+        case "${option}" in
+            "h") printScriptUsage ;;
+            "v") readonly verbose=true ;;
+            "x") readonly debug=true; set -x ;;
+            "c") setting_file_path="${OPTARG}" ;;
+            *) exitScript "ERROR : parameter invalid ! Use -h option to know more." 1 ;;
+        esac
+    done
+}
+
+# DESC: Main control flow
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: None
+function main() {
+    #+----------------------------------------------------------------------------------------------------------+
+    # Load utils
+    source "$(dirname "${BASH_SOURCE[0]}")/../../shared/lib/utils.bash"
+
+    #+----------------------------------------------------------------------------------------------------------+
+    # Init script
+    initScript "${@}"
+    parseScriptOptions "${@}"
+    loadScriptConfig "${setting_file_path-}"
+    redirectOutput "${cbna_log_imports}"
+
+    #+----------------------------------------------------------------------------------------------------------+
+    # Start script
+    printInfo "${app_name} script started at: ${fmt_time_start}"
+
+    echo 'Drop database "si_cbn" if exists'
+    dropdb --if-exists si_cbn
+    echo 'Create database "si_cbn"'
+    createdb -T template0 si_cbn
+    echo 'Create users'
+    createUser
+
+    read -r -n 1 key # Move to a new line
+
+    #+----------------------------------------------------------------------------------------------------------+
+    # Display script execution infos
+    displayTimeElapsed
+}
+
+function createUser() {
+    psql -h "${db_host}" -U "${db_user}" -d "${db_name}" \
+        # -v "${psql_var}=${table}"
+}
