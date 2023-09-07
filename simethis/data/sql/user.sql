cc1ae9fe0fc5b32caac75c670c015891b3f4ac29
Christine BIONDA <c.bionda@cbn-alpin.fr>
2023-09-07 16:53:30 +0200

    fix(simethis data sql): make sure that the unique_id is unique for both observers and users

--- simethis/data/sql/user.sql
+++ simethis/data/sql/user.sql
@@ -50,186 +50,192 @@ COPY (
 				OR  (rel.meta_id_groupe <> 1 AND rel.insee_dept IN ('04', '05', '01', '26', '38', '73', '74')))
 		)
 -- CBNA accreditation territory observers
-	SELECT DISTINCT ON (observers.unique_id) *
-	FROM (  	
-
-		SELECT
-			flore.check_cbna_agent(r.id_obs1, r.date_releve_deb) AS unique_id, --obs1
-			NULL AS identifier,
-			public.delete_space(o.prenom) AS firstname,
-			o.nom AS "name",
-			public.delete_space(o.email) AS email,
-			r.code_organism,
-			public.delete_space(o.comm) AS "comment",
-			NULL AS "enable",
-			jsonb_build_object(
-				'code', o.code,
-			    'idGroupe', o.meta_id_groupe, 
-				'anneeNai', o.annee_nai , 
-				'anneeDec', o.annee_dec, 
-				'idObs', r.id_obs1
-			)::jsonb AS additional_data,
-			o.meta_date_saisie::timestamp AS meta_create_date,
-			o.meta_date_maj::timestamp AS meta_update_date,
-			'I'::char(1) AS meta_last_action   	
-		FROM releves_sialp AS r
-			JOIN referentiels.observateur o ON o.id_obs = r.id_obs1 
+	SELECT DISTINCT ON (cbna_roles.unique_id) *
+	FROM ( 
+		SELECT DISTINCT ON (observers.unique_id) *
+		FROM (  	
+	
+			SELECT
+				flore.check_cbna_agent(r.id_obs1, r.date_releve_deb) AS unique_id, --obs1
+				NULL AS identifier,
+				public.delete_space(o.prenom) AS firstname,
+				o.nom AS "name",
+				public.delete_space(o.email) AS email,
+				r.code_organism,
+				public.delete_space(o.comm) AS "comment",
+				NULL AS "enable",
+				jsonb_build_object(
+					'code', o.code,
+				    'idGroupe', o.meta_id_groupe, 
+					'anneeNai', o.annee_nai , 
+					'anneeDec', o.annee_dec, 
+					'idObs', r.id_obs1
+				)::jsonb AS additional_data,
+				o.meta_date_saisie::timestamp AS meta_create_date,
+				o.meta_date_maj::timestamp AS meta_update_date,
+				'I'::char(1) AS meta_last_action   	
+			FROM releves_sialp AS r
+				JOIN referentiels.observateur o ON o.id_obs = r.id_obs1 
+				
+			UNION
 			
-		UNION
-		
-		SELECT
-			flore.check_cbna_agent(r.id_obs2, r.date_releve_deb ) AS unique_id, --obs2
-			NULL AS identifier,
-			public.delete_space(o.prenom) AS firstname,
-			o.nom AS "name",
-			public.delete_space(o.email) AS email,
-			r.code_organism,
-			public.delete_space(o.comm) AS "comment",
-			NULL AS "enable",
-			jsonb_build_object(
-				'code', o.code, 
-				'idGroupe', o.meta_id_groupe, 
-				'anneeNai', o.annee_nai , 
-				'anneeDec', o.annee_dec, 
-				'idObs', r.id_obs2
-			)::jsonb AS additional_data,
-			o.meta_date_saisie::timestamp AS meta_create_date,
-			o.meta_date_maj::timestamp AS meta_update_date,
-			'I'::char(1) AS meta_last_action
-		FROM releves_sialp AS r
-			JOIN referentiels.observateur o ON o.id_obs = r.id_obs2
-		
-		UNION
-		
-		SELECT
-			flore.check_cbna_agent(r.id_obs3, r.date_releve_deb) AS unique_id, --obs3
-			NULL AS identifier,
-			public.delete_space(o.prenom) AS firstname,
-			o.nom AS "name",
-			public.delete_space(o.email) AS email,
-			r.code_organism,
-			public.delete_space(o.comm) AS "comment",
-			NULL AS "enable",
-			jsonb_build_object(
-				'code', o.code, 
-				'idGroupe', o.meta_id_groupe, 
-				'anneeNai', o.annee_nai , 
-				'anneeDec', o.annee_dec,
-				'idObs', r.id_obs3
-			)::jsonb AS additional_data,
-			o.meta_date_saisie::timestamp AS meta_create_date,
-			o.meta_date_maj::timestamp AS meta_update_date,
-			'I'::char(1) AS meta_last_action
-		FROM releves_sialp AS r
-			JOIN referentiels.observateur o ON o.id_obs = r.id_obs3 
-		
-		UNION
-		
-		SELECT
-			flore.check_cbna_agent(r.id_obs4, r.date_releve_deb) AS unique_id, --obs4
-			NULL AS identifier,
-			public.delete_space(o.prenom) AS firstname,
-			o.nom AS "name",
-			public.delete_space(o.email) AS email,
-			r.code_organism,
-			public.delete_space(o.comm) AS "comment",
-			NULL AS "enable",
-			jsonb_build_object(
-				'code', o.code, 
-				'idGroupe', o.meta_id_groupe, 
-				'anneeNai', o.annee_nai , 
-				'anneeDec', o.annee_dec,
-				'idObs', r.id_obs4
-			)::jsonb AS additional_data,
-			o.meta_date_saisie::timestamp AS meta_create_date,
-			o.meta_date_maj::timestamp AS meta_update_date,
-			'I'::char(1) AS meta_last_action
-		FROM releves_sialp AS r
-			JOIN referentiels.observateur o ON o.id_obs = r.id_obs4 
+			SELECT
+				flore.check_cbna_agent(r.id_obs2, r.date_releve_deb ) AS unique_id, --obs2
+				NULL AS identifier,
+				public.delete_space(o.prenom) AS firstname,
+				o.nom AS "name",
+				public.delete_space(o.email) AS email,
+				r.code_organism,
+				public.delete_space(o.comm) AS "comment",
+				NULL AS "enable",
+				jsonb_build_object(
+					'code', o.code, 
+					'idGroupe', o.meta_id_groupe, 
+					'anneeNai', o.annee_nai , 
+					'anneeDec', o.annee_dec, 
+					'idObs', r.id_obs2
+				)::jsonb AS additional_data,
+				o.meta_date_saisie::timestamp AS meta_create_date,
+				o.meta_date_maj::timestamp AS meta_update_date,
+				'I'::char(1) AS meta_last_action
+			FROM releves_sialp AS r
+				JOIN referentiels.observateur o ON o.id_obs = r.id_obs2
 			
-		UNION
-		
-		SELECT
-			flore.check_cbna_agent(r.id_obs5, r.date_releve_deb) AS unique_id, --obs5
-			NULL AS identifier,
-			public.delete_space(o.prenom) AS firstname,
-			o.nom AS "name",
-			public.delete_space(o.email) AS email,
-			r.code_organism,
-			public.delete_space(o.comm) AS "comment",
-			NULL AS "enable",
-			jsonb_build_object(
-				'code', o.code, 
-				'idGroupe', o.meta_id_groupe, 
-				'anneeNai', o.annee_nai , 
-				'anneeDec', o.annee_dec,
-				'idObs', r.id_obs5
-			)::jsonb AS additional_data,
-			o.meta_date_saisie::timestamp AS meta_create_date,
-			o.meta_date_maj::timestamp AS meta_update_date,
-			'I'::char(1) AS meta_last_action
-		FROM releves_sialp AS r
-			JOIN referentiels.observateur o ON o.id_obs = r.id_obs5
+			UNION
 			
-		) AS observers
-
-	UNION
-
--- CBNA users
-	(
-	SELECT DISTINCT ON (u.permid)
-		u.permid AS unique_id,
-		u.login AS identifier,
-		public.delete_space(u.prenom) AS firstname,
-		u.nom AS "name",
-		public.delete_space(u.email) AS email,
-		COALESCE(o.uuid_national, o.permid::varchar) AS code_organism, 
-		public.delete_space(u.comm) AS "comment",
-		NULL AS "enable",
-		jsonb_build_object(
-			'code', u.code, 
-			'idGroupe', u.id_groupe, 
-			'password', u.pass, 
-			'lastLogin', u.last_login, 
-			'key', u."key"
-		)::jsonb AS additional_data,
-		u.meta_date_saisie::timestamp AS meta_create_date,
-		u.meta_date_maj::timestamp AS meta_update_date,
-		'I'::char(1) AS meta_last_action
-	FROM applications.utilisateur u
-	JOIN referentiels.organisme o ON o.id_org = u.id_org 
-	WHERE u.id_groupe = 1
-	)
-
-	UNION 
-
--- Users outside CBNA but whose observations are located in the accreditation territory
-	(
-	SELECT DISTINCT ON (u.permid) 
-		u.permid AS unique_id,
-		u.login AS identifier,
-		public.delete_space(u.prenom) AS firstname,
-		u.nom AS "name",
-		public.delete_space(u.email) AS email,
-		COALESCE(o.uuid_national, o.permid::varchar) AS code_organism, 
-		public.delete_space(u.comm) AS "comment",
-		NULL AS "enable",
-		jsonb_build_object(
-			'code', u.code, 
-			'idGroupe', u.id_groupe, 
-			'password', u.pass, 
-			'lastLogin', u.last_login, 
-			'key', u."key"
-		)::jsonb AS additional_data,
-		u.meta_date_saisie::timestamp AS meta_create_date,
-		u.meta_date_maj::timestamp AS meta_update_date,
-		'I'::char(1) AS meta_last_action
-	FROM applications.utilisateur u
-	JOIN flore.releve r ON r.meta_id_user_saisie  = u.id_user
-	JOIN referentiels.organisme o ON o.id_org = u.id_org 
-	WHERE
-		r.meta_id_groupe <> 1 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74')
-	)
+			SELECT
+				flore.check_cbna_agent(r.id_obs3, r.date_releve_deb) AS unique_id, --obs3
+				NULL AS identifier,
+				public.delete_space(o.prenom) AS firstname,
+				o.nom AS "name",
+				public.delete_space(o.email) AS email,
+				r.code_organism,
+				public.delete_space(o.comm) AS "comment",
+				NULL AS "enable",
+				jsonb_build_object(
+					'code', o.code, 
+					'idGroupe', o.meta_id_groupe, 
+					'anneeNai', o.annee_nai , 
+					'anneeDec', o.annee_dec,
+					'idObs', r.id_obs3
+				)::jsonb AS additional_data,
+				o.meta_date_saisie::timestamp AS meta_create_date,
+				o.meta_date_maj::timestamp AS meta_update_date,
+				'I'::char(1) AS meta_last_action
+			FROM releves_sialp AS r
+				JOIN referentiels.observateur o ON o.id_obs = r.id_obs3 
+			
+			UNION
+			
+			SELECT
+				flore.check_cbna_agent(r.id_obs4, r.date_releve_deb) AS unique_id, --obs4
+				NULL AS identifier,
+				public.delete_space(o.prenom) AS firstname,
+				o.nom AS "name",
+				public.delete_space(o.email) AS email,
+				r.code_organism,
+				public.delete_space(o.comm) AS "comment",
+				NULL AS "enable",
+				jsonb_build_object(
+					'code', o.code, 
+					'idGroupe', o.meta_id_groupe, 
+					'anneeNai', o.annee_nai , 
+					'anneeDec', o.annee_dec,
+					'idObs', r.id_obs4
+				)::jsonb AS additional_data,
+				o.meta_date_saisie::timestamp AS meta_create_date,
+				o.meta_date_maj::timestamp AS meta_update_date,
+				'I'::char(1) AS meta_last_action
+			FROM releves_sialp AS r
+				JOIN referentiels.observateur o ON o.id_obs = r.id_obs4 
+				
+			UNION
+			
+			SELECT
+				flore.check_cbna_agent(r.id_obs5, r.date_releve_deb) AS unique_id, --obs5
+				NULL AS identifier,
+				public.delete_space(o.prenom) AS firstname,
+				o.nom AS "name",
+				public.delete_space(o.email) AS email,
+				r.code_organism,
+				public.delete_space(o.comm) AS "comment",
+				NULL AS "enable",
+				jsonb_build_object(
+					'code', o.code, 
+					'idGroupe', o.meta_id_groupe, 
+					'anneeNai', o.annee_nai , 
+					'anneeDec', o.annee_dec,
+					'idObs', r.id_obs5
+				)::jsonb AS additional_data,
+				o.meta_date_saisie::timestamp AS meta_create_date,
+				o.meta_date_maj::timestamp AS meta_update_date,
+				'I'::char(1) AS meta_last_action
+			FROM releves_sialp AS r
+				JOIN referentiels.observateur o ON o.id_obs = r.id_obs5
+				
+			) AS observers 
+	
+		UNION
+	
+	-- CBNA users
+		SELECT DISTINCT ON (users.unique_id) *
+		FROM (
+			(
+			SELECT DISTINCT ON (u.permid)
+				u.permid AS unique_id,
+				u.login AS identifier,
+				public.delete_space(u.prenom) AS firstname,
+				u.nom AS "name",
+				public.delete_space(u.email) AS email,
+				COALESCE(o.uuid_national, o.permid::varchar) AS code_organism, 
+				public.delete_space(u.comm) AS "comment",
+				NULL AS "enable",
+				jsonb_build_object(
+					'code', u.code, 
+					'idGroupe', u.id_groupe, 
+					'password', u.pass, 
+					'lastLogin', u.last_login, 
+					'key', u."key"
+				)::jsonb AS additional_data,
+				u.meta_date_saisie::timestamp AS meta_create_date,
+				u.meta_date_maj::timestamp AS meta_update_date,
+				'I'::char(1) AS meta_last_action
+			FROM applications.utilisateur u
+			JOIN referentiels.organisme o ON o.id_org = u.id_org 
+			WHERE u.id_groupe = 1 
+			)
+	
+		UNION 
+	
+	-- Users outside CBNA but whose observations are located in the accreditation territory
+			(
+			SELECT DISTINCT ON (u.permid) 
+				u.permid AS unique_id,
+				u.login AS identifier,
+				public.delete_space(u.prenom) AS firstname,
+				u.nom AS "name",
+				public.delete_space(u.email) AS email,
+				COALESCE(o.uuid_national, o.permid::varchar) AS code_organism, 
+				public.delete_space(u.comm) AS "comment",
+				NULL AS "enable",
+				jsonb_build_object(
+					'code', u.code, 
+					'idGroupe', u.id_groupe, 
+					'password', u.pass, 
+					'lastLogin', u.last_login, 
+					'key', u."key"
+				)::jsonb AS additional_data,
+				u.meta_date_saisie::timestamp AS meta_create_date,
+				u.meta_date_maj::timestamp AS meta_update_date,
+				'I'::char(1) AS meta_last_action
+			FROM applications.utilisateur u
+			JOIN flore.releve r ON r.meta_id_user_saisie  = u.id_user
+			JOIN referentiels.organisme o ON o.id_org = u.id_org 
+			WHERE
+				r.meta_id_groupe <> 1 AND r.insee_dept IN ('04', '05', '01', '26', '38', '73', '74') 
+			)
+		)AS users
+	)AS cbna_roles
 ) TO '/tmp/user.csv' WITH(format csv, header, delimiter E'\t', null '\N');
 
 DROP TABLE IF EXISTS flore.cbna_agent;
