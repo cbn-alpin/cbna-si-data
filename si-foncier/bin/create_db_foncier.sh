dd2a6d5ad9bddb45c4d202c8599aa53bda2cba7b
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2023-10-25 16:31:50 +0200

    feat(foncier): download files, improve script code

--- si-foncier/bin/create_db_foncier.sh
+++ si-foncier/bin/create_db_foncier.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Encoding : UTF-8
-# Import some updated, deleted or new data in GeoNature Database the CBNA Flora Data.
+# Import Foncier data into sigeo database.
 
 #+----------------------------------------------------------------------------------------------------------+
 # Configure script execute options
@@ -61,71 +61,125 @@ function main() {
     parseScriptOptions "${@}"
     loadScriptConfig "${setting_file_path-}"
     redirectOutput "${log_file}"
-    local readonly commands=("7za" "psql" "dropdb" "createdb" "unzip")
-    checkBinary "${commands[@]}"
-
 
     #+----------------------------------------------------------------------------------------------------------+
     # Start script
     printInfo "${app_name} script started at: ${fmt_time_start}"
 
-    initDB
-    extractArchive "${raw_dir}"
-    restoreData
+    precheck
+    initializeDatabase
+    downloadArchives
+    unzipArchives
+    executeInitSqlFiles
+    executeDataSqlFiles
 
     #+----------------------------------------------------------------------------------------------------------+
     # Display script execution infos
     displayTimeElapsed
 }
 
-function initDB() {
+function precheck() {
+    printMsg "Check binaries and directories"
 
-    printMsg "create extension postgis & postgis_topology"
-    psql "${db_name}" --command "CREATE EXTENSION IF NOT EXISTS postgis; CREATE EXTENSION IF NOT EXISTS postgis_topology;"
-}
+    printVerbose "Check all necessary binaries" ${Gra-}
+    local readonly commands=("7za" "psql" "unzip" "gdown")
+    checkBinary "${commands[@]}"
 
-function extractArchive() {
-    printMsg "Extract import data CSV files..."
-
-    local dir="$1"
-
-    # Check if the directory exists
-    if [ ! -d "$dir" ]; then
-        echo "Directory '$dir' does not exist."
-        return 1
+    printVerbose "Check if the raw data directory exists" ${Gra-}
+    if [[ ! -d "${raw_dir}" ]]; then
+        exitScript "Directory '${raw_dir}' does not exist."
+    else
+        printVerbose "Found directory '${raw_dir}': ${Gre-}OK" ${Gra-}
     fi
 
-
-    # Loop through all archive files in the directory
-    find "$dir" -type f \( -name "*.tar" -o -name "*.tar.gz" -o -name "*.tgz" -o -name "*.tar.bz2" -o -name "*.tar.xz" -o -name "*.zip" -o -name "*.7z.001" -o -name "*.7z" \) -exec sh -c '
-        for archive do
-            if [ ! -d "${archive%.*}" ]; then
-                # Determine the archive format and extract accordingly
-                if [ "${archive##*.}" = "zip" ]; then
-                    unzip "$archive" -d "${archive%.*}"
-                    cd ${archive%.*}
-                    for archive in */; do
-                        7za x "$archive"
-                    done
-                    for archive in *.7z; do
-                        7za x "$archive" -o*
-                    done
-                else 
-                    tar xf "$archive" -C "${archive%.*}"
-                fi
-            else
-                echo "Zip file already extracted." ${Gra}
-            fi
-        done
-    ' sh {} +
 }
 
-function restoreData() {
-    printMsg "Import data into database..."
+function initializeDatabase() {
+    printMsg "Create extensions postgis & postgis_topology..."
+    psql "${db_name}" --command "CREATE EXTENSION IF NOT EXISTS postgis;"
+    psql "${db_name}" --command "CREATE EXTENSION IF NOT EXISTS postgis_topology;"
+}
 
-    find "${raw_dir}/" -type f -name "*.sql" -print0 | while read -d $'\0' file ; do
+function downloadArchives() {
+    printMsg "Download ${#sifo_gdrive_files[@]} necessary archives files from Google Drive..."
+    cd "${raw_dir}"
+    for file_id in "${!sifo_gdrive_files[@]}"; do
+        local file_name="${sifo_gdrive_files[$file_id]}"
+        if [[ ! -f "${file_name}" ]]; then
+            gdown --quiet --output "${file_name}" "${file_id}"
+        else
+            printVerbose "File '${file_name}' already downloaded !" ${Gra-}
+        fi
+    done
+}
+
+function unzipArchives() {
+    printMsg "Unzip archive files..."
+    # Loop through all archive files in the raw data directory
+    find "${raw_dir}" -type f -name "*.zip" -print0 | sort -z | while read -d $'\0' archive; do
+        printVerbose "Unziping archive: ${archive}" ${Gra-}
+        if [[ ! -d "${archive%.*}" ]]; then
+            # Determine the archive format and extract accordingly
+            unzip "${archive}" -d "${archive%.*}"
+            cd ${archive%.*}
+            pwd
+            for archive in */*.7z.001; do
+                7za x "${archive}"
+            done
+            for archive in *.7z; do
+                7za x "${archive}" -o*
+            done
+        else
+            printVerbose "Zip file '${archive}' already extracted." ${Gra-}
+        fi
+    done
+}
+
+function executeInitSqlFiles() {
+    printMsg "Extract distinct INIT SQL files..."
+    declare -A distinct_sql_files
+    # WARNING: do not use PIPE with associative array (distinct_sql_files)
+    while IFS= read -r -d '' file_path; do
+        local lines=$(wc -l "${file_path}" | awk '{ print $1 }')
+        local bytes=$(wc -c "${file_path}" | awk '{ print $1 }')
+        local hash="${lines}-${bytes}"
+
+        if [[ ! -n "${distinct_sql_files[${hash}]:-}" ]]; then
+            printVerbose "New distinct file find: ${hash} ${file_path}" ${Gra-}
+            distinct_sql_files[${hash}]="${file_path}"
+        fi
+    done < <(find "${raw_dir}/" -type f -name "*_init.sql" -print0)
+
+    printMsg "Execute ${#distinct_sql_files[@]} INIT SQL files..."
+    for file in "${distinct_sql_files[@]}"; do
+        printVerbose "Importing SQL file '${file}' ..." ${Gra-}
         export PGPASSWORD="${db_pass}"; \
-        psql --host "${db_host}" --port "${db_port}" --username "${db_user}" --dbname "${db_name}" --file "${file}"
+            psql --host "${db_host}" --port "${db_port}" --username "${db_user}" \
+                --dbname "${db_name}" --file "${file}"
+    done
+}
+
+function executeDataSqlFiles() {
+    printMsg "Extract distinct DATA SQL files..."
+    declare -A distinct_sql_files
+    # WARNING: do not use PIPE with associative array (distinct_sql_files)
+    while IFS= read -r -d '' file_path; do
+        local lines=$(wc -l "${file_path}" | awk '{ print $1 }')
+        local bytes=$(wc -c "${file_path}" | awk '{ print $1 }')
+        local hash="${lines}-${bytes}"
+
+        if [[ ! -n "${distinct_sql_files[${hash}]:-}" ]]; then
+            printVerbose "New distinct file find: ${hash} ${file_path}" ${Gra-}
+            distinct_sql_files[${hash}]="${file_path}"
+        fi
+    done < <(find "${raw_dir}/" -type f -name "*_data.sql" -print0)
+
+    printMsg "Execute ${#distinct_sql_files[@]} DATA SQL files..."
+    for file in "${distinct_sql_files[@]}"; do
+        printVerbose "Importing SQL file '${file}' ..." ${Gra-}
+        export PGPASSWORD="${db_pass}"; \
+            psql --host "${db_host}" --port "${db_port}" --username "${db_user}" \
+                --dbname "${db_name}" --file "${file}"
     done
 }
 
