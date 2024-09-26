cb35b8e1f15bfb235e1c87f98fbee10f4b435211
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2024-09-26 16:12:12 +0200

    fix: fix several points after JP comments

--- simethis/data/sql/blur_geom.sql
+++ simethis/data/sql/blur_geom.sql
@@ -1,9 +1,9 @@
--- Return 10km mesh geometry
 CREATE OR REPLACE FUNCTION flore.blur_geom(geom4326 geometry, idObs integer, cdSensitivity integer)
     RETURNS geometry
     LANGUAGE plpgsql
     SECURITY DEFINER
 AS $function$
+-- Return 10km mesh geometry
     DECLARE
         geom4326 geometry;
     BEGIN
@@ -13,15 +13,15 @@ AS $function$
                 	CASE
 	                	WHEN s.geom_maille10_4326 IS NOT NULL THEN s.geom_maille10_4326
 	                	ELSE (SELECT mi.geom_4326 FROM flore.releve r JOIN referentiels.maille_inpn_10 mi ON mi.geom_4326 && r.geom_4326)
-	                END
-                ELSE r.geom_4326
-            END
-        INTO geom4326
+                        END
+                    ELSE r.geom_4326
+                END
+            INTO geom4326
         FROM flore.releve r
-			JOIN flore.observation o
-				ON o.id_releve = r.id_releve
-			LEFT JOIN flore.synthese s
-				ON s.id_observation = o.id_observation
+            JOIN flore.observation o
+                ON o.id_releve = r.id_releve
+            LEFT JOIN flore.synthese s
+                ON s.id_observation = o.id_observation
         WHERE idObs = o.id_observation;
 
         RETURN geom4326;
