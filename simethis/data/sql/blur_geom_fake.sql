82b1bfae9758519e1a6b2cfd1745ec0fca0021c0
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2024-09-26 16:12:12 +0200

    fix: fix several points after JP comments

--- simethis/data/sql/blur_geom_fake.sql
+++ simethis/data/sql/blur_geom_fake.sql
@@ -1,11 +1,9 @@
-
--- Return precise geometry
-
 CREATE OR REPLACE FUNCTION flore.blur_geom(geom4326 geometry, idObs integer, cdSensitivity integer)
     RETURNS geometry
     LANGUAGE plpgsql
     SECURITY DEFINER
 AS $function$
+-- Return precise geometry
     BEGIN
         RETURN geom4326;
     END;
