28fffecc5147710cf038c2ad8721e7c1e2b1d878
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2025-07-03 11:45:02 +0200

    feat(occtax): update linkt between occtax and dataset

--- geonature/data/sql/post-data-integration.sql
+++ geonature/data/sql/post-data-integration.sql
@@ -877,19 +877,24 @@ INSERT INTO utilisateurs.cor_roles (
         AND identifiant IS NULL
 \echo 'Finalize integration of users in GN Siflora database'
 
+
 \echo '----------------------------------------------------------------------------'
 \echo 'Links datasets to module Occtax'
 -- Enables selection of datasets on module Occtax for data entry tests
--- TO DO: Remove this part after CBNA data is integrated into Occtax
-
+-- TODO: Remove this part after CBNA data is integrated into Occtax
 INSERT INTO gn_commons.cor_module_dataset (
-	id_module,
-	id_dataset
+    id_module,
+    id_dataset
 )
-	SELECT
-		gn_commons.get_id_module_bycode('OCCTAX'),
-		id_dataset
-	FROM gn_meta.t_datasets
+    SELECT
+        gn_commons.get_id_module_bycode('OCCTAX'),
+        d.id_dataset
+    FROM gn_meta.t_datasets AS d
+        JOIN gn_meta.cor_dataset_actor AS cda
+            ON d.id_dataset = cda.id_dataset
+    WHERE d."active" = TRUE
+        AND cda.id_nomenclature_actor_role = ref_nomenclatures.get_id_nomenclature('ROLE_ACTEUR', '6') -- Producteur
+        AND cda.id_organism = utilisateurs.get_id_organism_by_uuid('5a433bd0-1fc0-25d9-e053-2614a8c026f8') -- Conservatoire botanique national alpin;
 ON CONFLICT DO NOTHING ;
 
 \echo '----------------------------------------------------------------------------'
